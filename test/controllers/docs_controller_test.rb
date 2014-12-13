09e5d2b59359770d346298845b3b73a823566589
sanemat <o.gata.ken@gmail.com>
2014-12-14 08:17:57 +0900

    Exec `rubocop -a`

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class DocsControllerTest < ActionController::TestCase
-  test "should get hello" do
+  test 'should get hello' do
     get :hello
     assert_response :success
   end
-
 end
