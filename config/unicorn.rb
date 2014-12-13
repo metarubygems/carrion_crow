84e41f71721598ef10d3fc085d5ec58599ba20c1
sanemat <o.gata.ken@gmail.com>
2014-12-14 08:40:43 +0900

    Ignore unicorn conf from rubocop

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # https://devcenter.heroku.com/articles/getting-started-with-rails4
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
 
