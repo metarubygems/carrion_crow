b03eda627cdd236a682f08a6c140e14969317ba1
sanemat <o.gata.ken@gmail.com>
2014-12-14 21:51:21 +0900

    Add implementation fo compare

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -28,7 +28,19 @@ class Comparison
     result
   end
 
-  def execute
+  def build_url(name, version_from, version_to)
+    "/v1/diffs?from=#{name}-#{version_from}&to=#{name}-#{version_to}"
+  end
 
+  def execute
+    url = build_url(id, from, to)
+    conn = ::Faraday.new(:url => Rails.application.secrets.raw_api_server) do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  ::Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+    response = conn.get url
+    response.body
   end
 end
